24c555b645b39ec23742ebd5ba5f4da070a998e4
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-30 13:58:01 -0700

    Added framework and first HTTPPost.

---
+++ HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+  public class StylistController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistController(HairSalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylist.ToList();
+      return View(model);
+    }
+    
+    public ActionResult Create ()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create (Stylist stylist)
+    {
+      _db.Stylist.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult stylist_details(int id)
+    {
+      Stylist thisStylist = _db.Stylist.Include(stylist_details => stylist.Client).FirstOrDefault(Stylist => stylist_details.stylist_id == id);
+      return View(thisStylist);
+    }
+
+    public ActionResult (int id)
+    {
+      Stylist thisStylist = _db.Stylist.FirstOrDefault(stylist_details => stylist_details.stylist_id == id);
+      return View(thisStylist);
+    }
+  }
+}
